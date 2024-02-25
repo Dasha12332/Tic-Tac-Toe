6dda3fd72b698dfe61a917780248159fa5d794a9
Дарья <91193776+Dasha12332@users.noreply.github.com>
2024-02-25 21:48:00 +0300

    Add files via upload

---
+++ Уровень 1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Уровень_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(132, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сбросить счет";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(492, 457);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Новая игра";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Showcard Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(27, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(182, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Игрок 1: 0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Showcard Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(586, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(173, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Игрок 2: 0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox3.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.Yellow;
+            this.textBox3.Location = new System.Drawing.Point(308, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(169, 41);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = " Счет";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(217, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 360);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(782, 530);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Уровень 1/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Уровень_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pole = Start(pole);
+            Read(pole);
+            textBox1.Text = "Игрок 1:   " + pobed1;
+            textBox2.Text = "Игрок 2:   " + pobed2;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        static int[,] pole =new int[3, 3];
+        int x;
+        int y;
+        int step = 0;
+        int pobed1 = 0;
+        int pobed2 = 0;
+        string s;
+        int n = 0;
+            public void Read(int[,] pole)
+            {
+                StreamReader sr = new StreamReader("История.txt");
+                string[] s = sr.ReadLine().Split(" ");
+            if (s[s.Length - 1] != "end")
+            {
+                pobed1 =Int32.Parse(s[s.Length - 3]);
+                pobed2 = Int32.Parse(s[s.Length - 2]);
+                step = Int32.Parse(s[s.Length - 1]);
+                for (int i = 0; i < step; i++)
+                {
+                    pole[Int32.Parse(s[s.Length - 6 - i * 3]), Int32.Parse(s[s.Length - 5 - i * 3])] =Convert.ToInt32(s[s.Length - 4 - i * 3]);
+                }
+            }      
+                sr.Close();
+            }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 1; i < 3; i++)
+            {
+                g.DrawLine(new Pen(Color.White, 2.0f), 0, 120 * i, 360, 120 * i);
+                g.DrawLine(new Pen(Color.White, 2.0f), 120 * i, 0, 120 * i, 360);
+            }
+            xod(e);
+        }
+
+        public static int[,] Start(int[,] pole)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    pole[i, j] = 0;
+            return (pole);
+        }
+
+        private void xod(PaintEventArgs e)
+        {
+            if (n != 0)
+            {
+                if (pole[x, y] == 0)
+                {
+                    if (step % 2 == 1)
+
+                        pole[x, y] = 1;
+                    else
+                        pole[x, y] = 2;
+                }
+                else
+                    step = step - 1;
+                s = s + " " + x + " " + y + " " + pole[x, y];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (pole[i, j] == 1)
+                    {
+                        e.Graphics.DrawLine(new Pen(Color.Red, 4.0f), 20 + i * 120, 20 + j * 120, 100 + i * 120, 100 + j * 120);
+                        e.Graphics.DrawLine(new Pen(Color.Red, 4.0f), 100 + i * 120, 20 + j * 120, 20 + i * 120, 100 + j * 120);
+                    }
+                    if (pole[i, j] == 2)
+                    {
+                        e.Graphics.DrawEllipse(new Pen(Color.Blue, 4.0f), 24 * (1 + i * 5), 24 * (1 + j * 5), 70, 80);
+                    }
+                }
+            }
+            end(e);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            step++;
+            n++;
+            int clx = e.Location.X;
+            int cly = e.Location.Y;
+            for (int i = 0; i < 3; i++)
+            {
+                if ((i * 120 < cly) & (cly < (i + 1) * 120))
+                {
+                    y = i;
+                }
+                if ((i * 120 < clx) & (clx < (i + 1) * 120))
+                {
+                    x = i;
+                }
+                panel1.Invalidate();
+            }
+        }
+        private void end(PaintEventArgs e)
+        {
+            bool nichya=true;
+            for (int i = 0; i < 3; i++)
+            {
+                if ((pole[i, 0] == pole[i, 1]) & (pole[i, 1] == pole[i, 2]) & (pole[i, 0] != 0) |
+                    (pole[0, i] == pole[1, i]) & (pole[1, i] == pole[2, i]) & (pole[0, i] != 0))
+                {
+                    if ((pole[i, 0] == pole[i, 1]) & (pole[i, 1] == pole[i, 2]))
+                        e.Graphics.DrawLine(new Pen(Color.Green, 8.0f), 60 + i * 120, 10, 60 + i * 120, 350);
+                    else
+                        e.Graphics.DrawLine(new Pen(Color.Green, 8.0f), 10, 60 + i * 120, 350, 60 + i * 120);
+                    s = s + " " +pobed1+" "+pobed2+" "+ "end";
+                    nichya = false;
+                    if ((pole[i, 0] == 1) & (pole[i, 0] == pole[i, 1]) & (pole[i, 1] == pole[i, 2]) |
+                    (pole[0, i] == 1) & (pole[0, i] == pole[1, i]) & (pole[1, i] == pole[2, i]))
+                    {
+                        MessageBox.Show("Победил игрок 1!");
+                        pobed1++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Победил игрок 2!");
+                        pobed2++;
+                    }
+                }
+            }
+            
+            if ((pole[0, 0] == pole[1, 1]) & (pole[1, 1] == pole[2, 2])& (pole[0, 0] !=0))
+            {
+                e.Graphics.DrawLine(new Pen(Color.Green, 8.0f), 10, 10, 350, 350);
+                s = s + " " + pobed1 + " " + pobed2 + " " + "end";
+                nichya = false;
+                if (pole[1, 1] == 1)
+                {
+                    pobed1++;
+                    MessageBox.Show("Победил игрок 1!");
+                }
+                if (pole[1, 1] == 2)
+                {
+                    pobed2++;
+                    MessageBox.Show("Победил игрок 2!");
+                }
+            }
+            
+            if ((pole[0, 2] == pole[1, 1]) & (pole[1, 1] == pole[2, 0])&(pole[0, 2] !=0))
+            {
+                e.Graphics.DrawLine(new Pen(Color.Green, 8.0f), 350, 10, 10, 350);
+                s = s + " " + pobed1 + " " + pobed2 + " " + "end";
+                nichya = false;
+                if (pole[1, 1] == 1)
+                {
+                    pobed1++;
+                    MessageBox.Show("Победил игрок 1!");
+                }
+                if (pole[1, 1] == 2)
+                {
+                    pobed2++;
+                    MessageBox.Show("Победил игрок 2!");
+                }
+            }
+            
+            if ((step == 9)&(nichya == true))
+            {
+                MessageBox.Show("Ничья!");
+                s = s + " " + pobed1 + " " + pobed2 + " " + "end";
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pole = Start(pole);
+            textBox1.Text = "Игрок 1:   "+Convert.ToString(pobed1);
+            textBox2.Text = "Игрок 2:   " + Convert.ToString(pobed2);
+            step = 0;
+            n = 0;
+            panel1.Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pole = Start(pole);
+            textBox1.Text = "Игрок 1:   0";
+            textBox2.Text = "Игрок 2:   0";
+            pobed1 = 0;
+            pobed2 = 0;
+            step = 0;
+            n = 0;
+            panel1.Invalidate();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("История.txt");
+            if (s == null)
+            {
+                s = "end";
+            }
+            string[] S = s.Split(" ");
+            if (S[S.Length - 1] != "end")
+                s = s + " "+pobed1+" "+pobed2+" " + step;
+            sw.WriteLine(s);
+            sw.Close();
+        }
+    }
+}
